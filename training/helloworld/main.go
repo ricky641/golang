06fabb412edb92e939db9b06e5da87c7522c36dc
ricky641 <bhavesh.bansal@outlook.com>
2016-12-09 19:02:35 +0530

    hello world completed

--- training/helloworld/main.go
+++ training/helloworld/main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+
+	//for fmt reference click here https://godoc.org/fmt
+	fmt.Print("Hello from the other side")                           //without new line
+	fmt.Println("Hello from the other side")                         //with new line
+	fmt.Printf("%d \t %b \t %x \t %#x \t %q\n", 'A', 23, 23, 23, 65) //with formatted text
+
+	for i := 0; i < 100; i++ {
+		fmt.Printf("%d", i)
+	}
 }
